91b8785beca27144a39a20a818b3f0ffa018e693
evilgree <evilgree@yandex.ru>
2025-10-05 11:08:52 +0300

    ошибка чек стайл

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,7 +6,10 @@ import java.util.Collection;
 
 public interface UserStorage {
     User addUser(User user);
+
     User updateUser(User user);
+
     User getUserById(int id);
+    
     Collection<User> getAllUsers();
 }
